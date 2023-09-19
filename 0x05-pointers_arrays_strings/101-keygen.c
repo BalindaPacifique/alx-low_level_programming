1f688a0dd5fc66c27658165e3ecc37c574ca4b67
BalindaPacifique <you@example.com>
2023-09-19 14:45:43 -0700

    mm

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ * Return: 0 success,else 1 error
+ */
+int main(void)
+{
+
