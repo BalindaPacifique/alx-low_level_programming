77fa01b3b23e8a904ff47774ef13f95ddcf0db4f
BalindaPacifique <you@example.com>
2023-09-21 20:28:53 -0700

    late

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  a function to print a[2] = 98
+ * -You are not allowed to use the variable a in your new line of code
+ * -You can only write one statement
+ * -You are not allowed to use ,
+ * -You are not allowed to code anything else than the line of code
+ * -Your code should be written at line 19, before the ;
+ * -Do not remove anything from the initial code
+ * -and don’t change anything but the line of code you are adding
+ * Return: 0 success, elsee 1 error
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *k;
+
+	a[2] = 1024;
+	k = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * infinite_add - a function that adds two numbers.
+ * @n1: number one
+ * @n2: number two
+ * @r: pointer
+ * @size_r: variable
+ * Return: pointer
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
