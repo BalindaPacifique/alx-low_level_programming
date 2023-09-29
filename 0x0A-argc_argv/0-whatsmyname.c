4da96bc12a4b63e81ff9c8e7b7963a19c6ccc1a4
BalindaPacifique <you@example.com>
2023-09-29 16:12:19 -0700

    a program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - a function that prints its name
+ * @argc: variable
+ * @argv: ann array
+ * Return: 0 success, else 1
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
+
+
+#endif
