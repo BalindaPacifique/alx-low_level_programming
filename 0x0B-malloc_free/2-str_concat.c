247a326a8afaaf711a9bfc413239edcc2924bd77
BalindaPacifique <you@example.com>
2023-10-02 17:26:03 -0700

    concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,4 +9,43 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int s1length = 0;
+	int s2length = 0;
+	char *conca;
+	int i;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+		s1length++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		i++;
+		s2length++;
+	}
+	conca = malloc(sizeof(char) * (s1length + s2length) + 1);
+	if (conca == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		conca[i] = s1[i];
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		conca[s1length + i] = s2[i];
+	}
+	return (conca);
+}
