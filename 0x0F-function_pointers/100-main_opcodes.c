9d7dfe37d8186f1139dfe17ff33a87202dd69b4a
BalindaPacifique <you@example.com>
2023-10-11 14:46:18 -0700

    functions pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int n, j;
+	char *a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	a = (char *)main;
+
+	for (j = 0; j < n; j++)
+	{
+		if (i == n - 1)
+		{
+			printf("%02hhx\n", a[j]);
+			break;
+		}
+		printf("%02hhx ", a[j]);
+	}
+	return (0);
+}
